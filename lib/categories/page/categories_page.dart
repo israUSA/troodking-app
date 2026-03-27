39311dd6fe869aa72666a499fe0af30f715366aa
ISRAEL <leonardosellan20@gmail.com>
2026-03-27 16:14:17 -0500

    advances in categories module,

--- lib/categories/page/categories_page.dart
+++ lib/categories/page/categories_page.dart
@@ -1,6 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:troodking_app/categories/widgets/categories_card_widget.dart';
 import 'package:troodking_app/env/theme/app_theme.dart';
 import 'package:troodking_app/shared/helpers/global_helper.dart';
 import 'package:troodking_app/shared/helpers/responsive.dart';
@@ -39,57 +40,73 @@ class _CategoriesPageState extends State<CategoriesPage> {
           height: responsive.hp(80),
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // IconButtonWidget(
-              //   buttonText: 'Agregar Categoría',
-              //   iconData: Icons.add,
-              //   onPress: () {
-              //     final keyAddCategorieAlert = GlobalHelper.genKey();
-              //     fp.showAlert(
-              //       key: keyAddCategorieAlert, 
-              //       content: AlertGeneric(
-              //         content: AlertAddCategorieWidget(
-              //           keyToClose: keyAddCategorieAlert,
-              //           confirm: () {
+              IconButtonWidget(
+                buttonText: 'Agregar Categoría',
+                iconData: Icons.add,
+                onPress: () {
+                  final keyAddCategorieAlert = GlobalHelper.genKey();
+                  fp.showAlert(
+                    key: keyAddCategorieAlert, 
+                    content: AlertGeneric(
+                      content: AlertAddCategorieWidget(
+                        keyToClose: keyAddCategorieAlert,
+                        confirm: () {
                           
-              //           },
-              //           ),
-              //         ),
-              //       );
-              //     },
-              //   ),
-                InkWell(
-                  onTap: () {
-                    final keyAddCategorieAlert = GlobalHelper.genKey();
-                    fp.showAlert(
-                      key: keyAddCategorieAlert,
-                      content: AlertGeneric(
-                        content: AlertAddCategorieWidget(
-                          keyToClose: keyAddCategorieAlert,
-                          confirm: () {
-                            
-                          },
+                        },
                         ),
                       ),
                     );
                   },
-                  splashColor: AppTheme.proteinColor.withValues(alpha: 0.4),
-                  child: DottedBorder(
-                    options: RectDottedBorderOptions(
-                      dashPattern: [7, 5],
-                      strokeWidth: 4,
-                      padding: EdgeInsets.all(responsive.dp(6)),
-                      color: AppTheme.primaryColor,
-                    ),
-                    child: Column(
-                      children: [
-                        Icon(Icons.add, color: AppTheme.primaryColor, size: responsive.dp(4),),
-                        SizedBox(height: responsive.hp(1),),
-                        TitleWidget(title: 'Agregar Categoría', fontSize: responsive.dp(2.2),)
-                      ],
-                    )),
-                )
+                ),
+                // InkWell(
+                //   onTap: () {
+                //     final keyAddCategorieAlert = GlobalHelper.genKey();
+                //     fp.showAlert(
+                //       key: keyAddCategorieAlert,
+                //       content: AlertGeneric(
+                //         content: AlertAddCategorieWidget(
+                //           keyToClose: keyAddCategorieAlert,
+                //           confirm: () {
+
+                //           },
+                //         ),
+                //       ),
+                //     );
+                //   },
+                //   splashColor: AppTheme.proteinColor.withValues(alpha: 0.4),
+                //   child: DottedBorder(
+                //     options: RectDottedBorderOptions(
+                //       dashPattern: [7, 5],
+                //       strokeWidth: 4,
+                //       padding: EdgeInsets.all(responsive.dp(6)),
+                //       color: AppTheme.primaryColor,
+                //     ),
+                //     child: Column(
+                //       children: [
+                //         Icon(Icons.add, color: AppTheme.primaryColor, size: responsive.dp(4),),
+                //         SizedBox(height: responsive.hp(1),),
+                //         TitleWidget(title: 'Agregar Categoría', fontSize: responsive.dp(2.2),)
+                //       ],
+                //     )),
+                // ),
+                GridView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    ), 
+                  itemBuilder: (context, index) {
+                    return CategoriesCardWidget(
+                      onPressed: () {
+                        
+                      }, 
+                      title: 'Pasta', 
+                      emojiIcon: '🍝');
+                  },)
               ],
             ),
         ),
--- lib/categories/widgets/categories_card_widget.dart
+++ lib/categories/widgets/categories_card_widget.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:troodking_app/env/theme/app_theme.dart';
+import 'package:troodking_app/shared/helpers/responsive.dart';
+import 'package:troodking_app/shared/widgets/title.dart';
 
 class CategoriesCardWidget extends StatefulWidget {
-  const CategoriesCardWidget({super.key, required this.onPressed, required this.title});
+  const CategoriesCardWidget({super.key, required this.onPressed, required this.title, required this.emojiIcon});
 
   final String title;
+  final String emojiIcon;
   final void Function() onPressed;
   
 
@@ -15,12 +19,27 @@ class CategoriesCardWidget extends StatefulWidget {
 class _CategoriesCardWidgetState extends State<CategoriesCardWidget> {
   @override
   Widget build(BuildContext context) {
+    final responsive = Responsive(context);
     return Card(
+      color: AppTheme.white,
       child: InkWell(
         onTap: () {
           
         },
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: responsive.hp(6),
+              child: Text(
+                widget.emojiIcon,
+                style: TextStyle(
+                  fontSize: responsive.dp(5)
+                ),
+              ),
+            ),
+            TitleWidget(title: widget.title)
+          ],
           
         ),
       ),

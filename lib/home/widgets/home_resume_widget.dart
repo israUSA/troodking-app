9c8f90a1c8b9820b52a0110411333cf9bfd6921c
ISRAEL <leonardosellan20@gmail.com>
2026-04-14 08:36:16 -0500

    advances in resumen

--- lib/home/widgets/home_resume_widget.dart
+++ lib/home/widgets/home_resume_widget.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,12 +12,13 @@ import 'package:troodking_app/shared/widgets/title.dart';
 class HomeResumeWidget extends StatefulWidget {
   const HomeResumeWidget({super.key});
 
-
   @override
   State<HomeResumeWidget> createState() => _HomeResumeWidgetState();
 }
 
 class _HomeResumeWidgetState extends State<HomeResumeWidget> {
+  final CarouselSliderController _controller = CarouselSliderController();
+  int currentStep = 0;
 
   @override
   void initState() {
@@ -29,30 +31,77 @@ class _HomeResumeWidgetState extends State<HomeResumeWidget> {
     return Consumer<FunctionalProvider>(
       builder: (context, fp, child) {
         return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          
-          TitleWidget(title: 'Resúmen', fontSize: responsive.dp(2.3),  fontWeight: FontWeight.bold,),
-          SizedBox(height: responsive.hp(2),),
-          DottedBorder(
-            options: RectDottedBorderOptions(
-              dashPattern: [10, 5],
-              strokeWidth: 3,
-              color: AppTheme.carbsColor
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TitleWidget(
+              title: 'Resúmen',
+              fontSize: responsive.dp(2.3),
+              fontWeight: FontWeight.bold,
             ),
-            child: fp.troodkingModel.isNotEmpty
-            ? Column(
-              children: [
-                TextWidget(title: 'Se agrego ${fp.troodkingModel.last.categorieName} como categoría')
-      
-              ],
-            )
-            : EmptyDataMessageWidget(message: 'Crea una categoría e ingresa un producto'),
-
-            
-          )
-        ],
-      );
+            SizedBox(height: responsive.hp(2)),
+            fp.troodkingModel.isNotEmpty
+                ? Column(
+                    children: [
+                      CarouselSlider(
+                        options: CarouselOptions(
+                          height: 200.0,
+                          onPageChanged: (index, reason) {
+                            currentStep = index;
+                            setState(() {});
+                          },
+                          viewportFraction: 1,
+                          autoPlay: true,
+                          autoPlayInterval: const Duration(milliseconds: 4000),
+                          enlargeCenterPage: true,
+                        ),
+                        items: [1, 2, 3, 4, 5].map((i) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(color: Colors.amber),
+                                child: Text(
+                                  'text $i',
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                              );
+                            },
+                          );
+                        }).toList(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [1, 2, 3, 4, 5].asMap().entries.map((entry) {
+                          return GestureDetector(
+                            onTap: () => _controller.animateToPage(entry.key),
+                            child: Container(
+                              width: 12.0,
+                              height: 12.0,
+                              margin: EdgeInsets.symmetric(
+                                vertical: 8.0,
+                                horizontal: 4.0,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: (Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black)
+                                        .withValues(
+                                          alpha: currentStep == entry.key
+                                              ? 0.9
+                                              : 0.4,
+                                        ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  )
+                : EmptyDataMessageWidget(
+                    message: 'Crea una categoría e ingresa un producto',
+                  ),
+          ],
+        );
       },
     );
   }
